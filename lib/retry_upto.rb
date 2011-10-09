6dff200658f997cadd95e8cd9d0623b69f8a5ee5
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-10-09 13:42:40 +0200

    :growth is no longer an individual option

--- lib/retry_upto.rb
+++ lib/retry_upto.rb
@@ -5,15 +5,15 @@ def retry_upto(max_retries = 1, options = {})
 rescue *(options[:rescue] || Exception)
   raise if (max_retries -= 1) == 0
 
-  if options[:interval].is_a?(Array) && options[:interval].length == 2
-    options[:interval], options[:growth] = *options[:interval]
+  if Array(options[:interval]).length == 2
+    options[:interval], growth = *options[:interval]
   end
 
   sleep(options[:interval] || 0)
-  if options[:growth].respond_to?('*')
-    options[:interval] = options[:interval] * options[:growth]
-  elsif options[:growth].respond_to?(:call)
-    options[:interval] = options[:growth].call(options[:interval])
+  if growth.respond_to?('*')
+    options[:interval] *= growth
+  elsif growth.respond_to?(:call)
+    options[:interval]  = growth.call(options[:interval])
   end
   retry
 end
